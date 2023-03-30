95056e639c0133daa535c6a1932b68fc8410bb91
DutchEllie <personal@quenten.nl>
2023-03-30 14:19:53 +0200

    Made it work only when all localstorage is enabled

--- static/app.js
+++ static/app.js
@@ -8,86 +8,86 @@ Things Javascript does:
 */
 
 // This is only all of the input submits in the individual forms, because the final one is a button
-const allSubmitInputs = document.querySelectorAll('input[type="submit"]');
-const allForms = document.querySelectorAll('form');
-
-allSubmitInputs.forEach(elem => {
-	elem.classList.add("invisible");
-});
-
-// const allInputElements = document.querySelectorAll('input[type="text"], input[type="number"], input[type="email"] input[type="checkbox"], ')
-
-/*
-Form data storage format
-{
-	forms: {
-		<formid>: {
-			<elementname>: <value>
-		},
-		<formid>: {
-			<elementname>: <value>
-		},
-	}
-}
-
-*/
-
-var formData = {
-	forms: {}
-}
-
-// formID is a string, the ID attribute of the form
-function generateJsonFromForm(formID) {
-	var form = {}
-	const elements = Array.from(document.querySelectorAll(`#${formID} input:not([type="submit"]), #${formID} select`));
-	elements.forEach(elem => {
-		const elementName = elem.getAttribute('name');
-		const elementValue = elem.value;
-		form[elementName] = elementValue;
-	})
-
-	formData.forms[formID] = form;
-	// console.log(formData);
-
-	const formJSON = JSON.stringify(form);
-	return formJSON;
-}
-
-// Check beforehand whether localStorage works or not!
-async function putFormDataInLocalStorage(key, formJSON) {
-	window.localStorage.setItem(key, formJSON);
-}
-
-// Check beforehand whether or not localStorage is enabled
-function putFormInputValuesBackFromLocalStorage() {
-	// Take all forms from document
-	// Check if the form's data is stored in localStorage already
-	// If it is, put it back
-	allForms.forEach(elem => {
-		const formID = elem.getAttribute('id');
-		if (window.localStorage.getItem(formID)) {
-			/*
-			The same format as a singular "form" element from formData above.
-			*/
-			const localStorageData = JSON.parse(window.localStorage.getItem(formID));
-			// const elements = Array.form(document.querySelectorAll(`#${formID} input:not([type="submit"]), #${formID} select`));
-			// elements.forEach(elem2 => {
-			// 	if(elem2.)
-			// })
-			Object.keys(localStorageData).forEach(key => {
-				// inputElement is the actual input/select element that "key" is the name of.
-				const inputElement = document.querySelector(`#${formID} input[name="${key}"], #${formID} select[name="${key}"]`);
-
-				inputElement.value = localStorageData[key];
-			})
-			
-		}
-	})
-}
-putFormInputValuesBackFromLocalStorage()
-
 // TODO: move this if statement around everything here
 if (window.localStorage) {
+	const allSubmitInputs = document.querySelectorAll('input[type="submit"]');
+	const allForms = document.querySelectorAll('form');
+
+	allSubmitInputs.forEach(elem => {
+		elem.classList.add("invisible");
+	});
+
+	// const allInputElements = document.querySelectorAll('input[type="text"], input[type="number"], input[type="email"] input[type="checkbox"], ')
+
+	/*
+	Form data storage format
+	{
+		forms: {
+			<formid>: {
+				<elementname>: <value>
+			},
+			<formid>: {
+				<elementname>: <value>
+			},
+		}
+	}
+
+	*/
+
+	var formData = {
+		forms: {}
+	}
+
+	// formID is a string, the ID attribute of the form
+	function generateJsonFromForm(formID) {
+		var form = {}
+		const elements = Array.from(document.querySelectorAll(`#${formID} input:not([type="submit"]), #${formID} select`));
+		elements.forEach(elem => {
+			const elementName = elem.getAttribute('name');
+			const elementValue = elem.value;
+			form[elementName] = elementValue;
+		})
+
+		formData.forms[formID] = form;
+		// console.log(formData);
+
+		const formJSON = JSON.stringify(form);
+		return formJSON;
+	}
+
+	// Check beforehand whether localStorage works or not!
+	async function putFormDataInLocalStorage(key, formJSON) {
+		window.localStorage.setItem(key, formJSON);
+	}
+
+	// Check beforehand whether or not localStorage is enabled
+	function putFormInputValuesBackFromLocalStorage() {
+		// Take all forms from document
+		// Check if the form's data is stored in localStorage already
+		// If it is, put it back
+		allForms.forEach(elem => {
+			const formID = elem.getAttribute('id');
+			if (window.localStorage.getItem(formID)) {
+				/*
+				The same format as a singular "form" element from formData above.
+				*/
+				const localStorageData = JSON.parse(window.localStorage.getItem(formID));
+				// const elements = Array.form(document.querySelectorAll(`#${formID} input:not([type="submit"]), #${formID} select`));
+				// elements.forEach(elem2 => {
+				// 	if(elem2.)
+				// })
+				Object.keys(localStorageData).forEach(key => {
+					// inputElement is the actual input/select element that "key" is the name of.
+					const inputElement = document.querySelector(`#${formID} input[name="${key}"], #${formID} select[name="${key}"]`);
+
+					inputElement.value = localStorageData[key];
+				})
+				
+			}
+		})
+	}
+	putFormInputValuesBackFromLocalStorage()
+
 	allForms.forEach(elem => {
 		elem.addEventListener('input', event => {
 			const formID = elem.getAttribute('id');
@@ -98,6 +98,7 @@ if (window.localStorage) {
 		})
 	})
 }
+
 
 
 // window.addEventListener("load", (event) => {
